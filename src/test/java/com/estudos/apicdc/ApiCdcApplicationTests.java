04a8028d6f73fa4a0a05787a9148240d6b002d1b
Henrique Lopes <henrique.nobrega@ccc.ufcg.edu.br>
2020-11-09 08:30:29 -0300

    Initial commit

---
+++ src/test/java/com/estudos/apicdc/ApiCdcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.apicdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCdcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
